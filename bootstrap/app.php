f48e30bb428d0fb265e70090afd37cf241a2c6ab
suceleste <susu.descamps@gmail.com>
2025-10-13 11:26:54 +0200

    Revert "Merge pull request #3 from suceleste/feature/setup-laravel12-cors"
    This reverts commit 1de27ec77b1d032ddea1f4207c86bc2ec11a6d5b, reversing
    changes made to 93e60e1dc8aec045c221464577ff02cb8864de36.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,35 +6,12 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: base_path('routes/web.php'),
-        api: base_path('routes/api.php'), // On active les routes API
-        commands: base_path('routes/console.php'),
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
         health: '/up',
-        // On configure les permissions pour les routes API
-        apiPrefix: 'api',
-        then: function () {
-            // On s'assure que les routes API demandent une authentification (pour plus tard)
-            Broadcast::routes();
-        }
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        // On définit les alias de middleware si nécessaire (ne pas toucher)
-        $middleware->alias([
-            'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
-        ]);
-
-        // On définit les règles CORS globales
-        $middleware->redirectGuestsTo(fn () => route('login'));
-        $middleware->validateCsrfTokens(except: [
-            'stripe/*',
-        ]);
-        $middleware->validateSignatures(except: [
-            'stripe/*',
-        ]);
-
-        // C'EST LA PARTIE LA PLUS IMPORTANTE
-        // On autorise notre frontend à communiquer avec l'API
-        $middleware->statefulApi();
+    ->withMiddleware(function (Middleware $middleware): void {
+        //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
